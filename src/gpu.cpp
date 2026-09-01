27721e12544bd3c7082ab34c404f3bcdfe605e6d
Simon Long <simon@raspberrypi.com>
2026-09-01 11:30:40 +0100

    Misc tidying

--- src/gpu.cpp
+++ src/gpu.cpp
@@ -50,7 +50,7 @@ void WidgetGPU::widget_config_reload (void)
 void WidgetGPU::widget_init (Gtk::HBox *container)
 {
     /* Create the button */
-    plugin = std::make_unique <Gtk::Button> ();
+    plugin = std::make_unique <Gtk::EventBox> ();
     plugin->set_name (PLUGIN_NAME);
     container->pack_start (*plugin, false, false);
 
